6e4b726296e7a02ca6a1196bacb2bfcf156d3f90
liqtranq <liqtranq@gmail.com>
2026-09-07 13:29:48 +0300

    Bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.liquidloop.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
