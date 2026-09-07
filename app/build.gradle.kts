b95290df94e40b7c6e6b0cd57505e3b16a3dae67
liqtranq <liqtranq@gmail.com>
2026-09-07 17:59:43 +0300

    Release v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.liquidloop.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
